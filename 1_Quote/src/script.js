b0001f66e2a7ddd7704821488e2f891413223d86
obtusedev <66740598+obtusedev@users.noreply.github.com>
2022-04-10 01:45:35 -0400

    1_Quote - Add automatic dark mode

--- 1_Quote/src/script.js
+++ 1_Quote/src/script.js
@@ -4,7 +4,20 @@ const previous = document.getElementById("previous");
 const random = document.getElementById("random");
 const next = document.getElementById("next");
 
+/* Dark Mode */
+const isTimeForDarkMode = () => {
+    // between 8:00pm & 6:00am; 20:00-06:00
+    const hour = new Date().getHours();
+    if (hour >= 20 || hour <= 6) {
+        return true;
+    }
+    return false;
+};
 
+const setDarkTheme = () => {
+    const bodyEl = document.querySelector("body");
+    if (isTimeForDarkMode()) bodyEl.setAttribute("class", "dark");
+};
 
 const searchWiki = e => {
     let query = e.target.textContent;
