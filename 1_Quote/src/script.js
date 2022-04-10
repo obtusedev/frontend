5f6292855c18206ef1cf5c5af2b04185eecdf76b
obtusedev <66740598+obtusedev@users.noreply.github.com>
2022-04-10 01:49:24 -0400

    1_Quote - Add checking for new day and create element
    - Add function to check if it is new day to make new request or use the one in local session.
    This reduces the number of api calls.
    - Creates the author span element.

--- 1_Quote/src/script.js
+++ 1_Quote/src/script.js
@@ -32,6 +32,26 @@ const copyQuoteToClipboard = e => {
     navigator.clipboard.writeText(e.target.textContent);
     alert("Copied!"); // TODO: make this more user friendly
 };
+
+const isNewDay = () => {
+    // does the quote of the day in local session cache need refreshing
+    const savedDate = new Date().getDate();
+    window.localStorage.setItem("day", savedDate);
+    const today = new Date().getDate();
+
+    if (window.localStorage.getItem("day") < today) return true;
+    return false;
+};
+
+const createAuthorSpan = author => {
+    const fragment = document.createElement("span");
+    fragment.setAttribute("id", "author");
+    fragment.setAttribute("class", "underline");
+    fragment.innerText = author;
+    fragment.onclick = searchWiki;
+    quoteEl.insertAdjacentElement("beforeend", fragment);
+};
+
 }
 
 authorEl.onclick = searchWiki;
